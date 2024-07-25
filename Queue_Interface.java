ad31aca4d6e4179ca4e9e3810d9204f4d7396a92
bhagyashree <bhaggo30july@gmail.com>
2024-07-25 16:25:02 +0530

    check the some implementation of priorityQueue class of Queue interfac

---
+++ Queue_Interface.java
@@ -0,0 +1,22 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Queue_Interface {
+
+    public static void main(String[] args) {
+
+        Queue <String>queue= new PriorityQueue<String>();
+        queue.add("Tom");
+        queue.add("jerry");
+        queue.add("jack");
+        queue.add("smith");
+        queue.add("john");
+
+        System.out.println("printing the elements in the array"+queue);
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        System.out.println(queue.offer("jack"));
+        System.out.println(queue.isEmpty());
+        System.out.println(queue);
+    }
+}
