47f1c2cd5222c19a6d059a83f862d7f6849f4ceb
bhagyashree <bhaggo30july@gmail.com>
2024-07-28 23:16:11 +0530

    in iterator class how methods works and iterate the object

--- IteratorClass.java
+++ IteratorClass.java
@@ -1,16 +1,25 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 
 public class IteratorClass {
     public static void main(String[] args) {
-        ArrayList al = new ArrayList<>();
+        ArrayList<Integer> al = new ArrayList<>();
         al.add(10);
         al.add(20);
         al.add(30);
         al.add(40);
         al.add(50);
 
-         Iterator itr = al.iterator();
+        Iterator<Integer> itr = al.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+        System.out.println("-----------");
 
+        ListIterator<Integer> it = al.listIterator(al.size());
+        while (it.hasPrevious()) {
+            System.out.println(it.previous());
+        }
     }
 }
