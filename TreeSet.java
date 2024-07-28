125bc4daf2d8aab9301d8c5340a2a1100c6dbff3
bhagyashree <bhaggo30july@gmail.com>
2024-07-28 22:59:18 +0530

    how TreeSet internally works

--- TreeSet.java
+++ TreeSet.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class TreeSet {
+public class TreeSet<S> {
+
+    public static void main(String[] args) {
+        String x ="a";
+        String y ="b";
+        System.out.println(x.compareTo(y));
+        System.out.println(y.compareTo(x));
+        System.out.println(x.compareTo(x));
+
+
+
+    }
 }
