82075a9e485bc4bc446385c537afae7d2891fe6b
bhagyashree <bhaggo30july@gmail.com>
2024-07-28 23:23:27 +0530

    sorting with the help of comparable interface

--- Comparable_Interface.java
+++ Comparable_Interface.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Comparable_Interface {
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+class Student implements Comparable<Student>{
+
+    int id;
+    String name;
+
+    Student(int id,String name){
+        this.id = id;
+        this.name = name;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student s) {
+        return this.id-s.id;
+    }
+}
+public class Comparable_Interface {
+    public static void main(String[] args) {
+        Student s1 = new Student(101,"tom");
+        Student s2 = new Student(102,"jerry");
+        Student s3 = new Student(103 ,"jack");
+        Student s4 = new Student(101,"smith");
+
+        Set<Student> set = new TreeSet<>();
+        set.add(s1);
+        set.add(s2);
+        set.add(s3);
+        set.add(s4);
+
+         Iterator itr= set.iterator();
+         while (itr.hasNext()){
+             System.out.println(itr.next());
+         }
+    }
 }
--- Comparator_Interface.java
+++ Comparator_Interface.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Comparator_Interface {
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+class Employee{
+    int id;
+    String name;
+
+    public Employee(int id) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+class EmployeeById implements Comparator<Employee>{
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return Integer.compare(o1.id ,o2.id);
+        // return o1.id - o2.id;
+    }
+}
+public class Comparator_Interface {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee(101);
+        Employee emp2 = new Employee(102);
+        Employee emp3 = new Employee(101);
+
+        Set<Employee> set = new TreeSet<>(new EmployeeById());
+        set.add(emp1);
+        set.add(emp2);
+        set.add(emp3);
+        Iterator itr= set.iterator();
+        while (itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
 }
