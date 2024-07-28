076669e94381cd8bee95bd18318b86d2a903f0c0
bhagyashree <bhaggo30july@gmail.com>
2024-07-28 23:11:45 +0530

    print only homogenous object

--- OnlyStoreHomogenousObject.java
+++ OnlyStoreHomogenousObject.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class OnlyStoreHomogenousObject {
+import java.util.ArrayList;
+
+public class OnlyStoreHomogenousObject {
+
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add("tom");
+        al.add(20);
+        al.add('a');
+        al.add(8.9);
+        al.add(10);
+        al.add(20);
+
+        for (Object obj :al){
+            if (obj instanceof Integer){
+                System.out.println(obj);
+            }
+        }
+    }
 }
