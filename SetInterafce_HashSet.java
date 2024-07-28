3a6ef4cc3b9f30a99a0e5ce3c4f91515fe3311fa
bhagyashree <bhaggo30july@gmail.com>
2024-07-28 23:01:01 +0530

    how set interface sort the object

--- SetInterafce_HashSet.java
+++ SetInterafce_HashSet.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class SetInterafce_HashSet {
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class SetInterafce_HashSet {
+    // sorting in without any sequence and set doesnt allow duplicate
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("abc");
+        set.add("mno");
+        set.add("ccc");
+        set.add("ccc");
+        Iterator itr =set.iterator();
+        while (itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
 }
