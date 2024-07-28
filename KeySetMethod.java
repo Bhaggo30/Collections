8d3dc5a7a9e3811e26f79c54bfa634b8698100e6
bhagyashree <bhaggo30july@gmail.com>
2024-07-28 23:13:57 +0530

    how keyset iterate object

--- KeySetMethod.java
+++ KeySetMethod.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class KeySetMethod {
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class KeySetMethod {
+    public static void main(String[] args) {
+        Map<Integer,String> map = new LinkedHashMap<>();
+        map.put(101,"apple");
+        map.put(102,"banana");
+        map.put(103,"orange");
+        map.put(101,"mango");
+        map.put(102,"grapes");
+         Set <Integer> keyset= map.keySet();
+        for (int key :keyset){
+            System.out.println("key of the fruit is "+key + " and the value is "+map.get(key));
+        }
+    }
 }
