5f41afb52f17fa1094d9030bdaf6b52a21362cdb
bhagyashree <bhaggo30july@gmail.com>
2024-07-28 23:12:54 +0530

    some implemented method of map interface

--- MapCollectionMethods.java
+++ MapCollectionMethods.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class MapCollectionMethods {
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapCollectionMethods {
+
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>();
+        map.put(101,"java");
+        map.put(102,"sql");
+        map.put(103,"javascript");
+
+        System.out.println(map);
+        System.out.println(map.get(10));
+        System.out.println(map.containsKey(101));
+        System.out.println(map.containsValue("java"));
+        System.out.println(map.size());
+        System.out.println(map);
+        map.remove(103);
+        System.out.println(map);
+        System.out.println(map.isEmpty());
+        map.clear();
+        System.out.println(map.isEmpty());
+    }
 }
