41253c698318cbb650020ba05cc51f7b00895690
bhagyashree <bhaggo30july@gmail.com>
2024-09-10 13:35:57 +0530

    grouped anagram using collection

---
+++ GroupAnagrams.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            int[] charCount = new int[26];
+            for (char c : str.toCharArray()) {
+                charCount[c - 'a']++;  // Count each letter
+            }
+
+            StringBuilder keyBuilder = new StringBuilder();
+            for (int count : charCount) {
+                keyBuilder.append(count).append("#");
+            }
+            String key = keyBuilder.toString();
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(input));
+    }
+}
